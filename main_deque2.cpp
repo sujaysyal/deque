1199d3521e97923b824add75f9f1a2055ce48e6a
Sujay Syal <52260748+sujaysyal@users.noreply.github.com>
2023-05-16 20:27:42 -0700

    Update main_deque2.cpp

--- main_deque2.cpp
+++ main_deque2.cpp
@@ -1,50 +1,50 @@
-//#include <iostream>
-//#include <functional>
-//#include <algorithm>
-//#include "Mydeque.h"
-//using namespace std;
-//
-//int main()
-//{
-//    Mydeque<char> sd;
-//   
-//    cout << "********** add elements *********" << endl;
-//    // add elements
-//    sd.push_front('5');
-//    sd.push_front('4');
-//    sd.push_front('3');
-//    sd.push_back('6');
-//    sd.push_back('7');
-//    sd.push_front('2');
-//    sd.push_front('1');
-//    sd.push_back('8');
-//    sd.push_back('9');
-//    sd.push_front('0');
-//    sd.push_front('-');
-//    sd.push_back('A');
-//    sd.push_back('B');
-//    
-//    cout << "********* deque (must appear in this order) **********" << endl;
-//    sd.print();
-//    cout << "********* real array **********" << endl;
-//    sd.real_print();
-//
-//      
-//    // remove elements
-//    sd.pop_back();
-//    sd.pop_front();
-//    sd.pop_back();
-//    sd.pop_front();
-//    sd.pop_back();
-//    sd.pop_front();
-//    sd.pop_back();
-//    cout << "********* deque (must appear in this order) **********" << endl;
-//    sd.print();
-//    cout << "********* real array **********" << endl;
-//    sd.real_print();
-//    
-//     
-//    return 0;
-//}
-//
-//
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include "Mydeque.h"
+using namespace std;
+
+int main()
+{
+   Mydeque<char> sd;
+  
+   cout << "********** add elements *********" << endl;
+   // add elements
+   sd.push_front('5');
+   sd.push_front('4');
+   sd.push_front('3');
+   sd.push_back('6');
+   sd.push_back('7');
+   sd.push_front('2');
+   sd.push_front('1');
+   sd.push_back('8');
+   sd.push_back('9');
+   sd.push_front('0');
+   sd.push_front('-');
+   sd.push_back('A');
+   sd.push_back('B');
+   
+   cout << "********* deque (must appear in this order) **********" << endl;
+   sd.print();
+   cout << "********* real array **********" << endl;
+   sd.real_print();
+
+     
+   // remove elements
+   sd.pop_back();
+   sd.pop_front();
+   sd.pop_back();
+   sd.pop_front();
+   sd.pop_back();
+   sd.pop_front();
+   sd.pop_back();
+   cout << "********* deque (must appear in this order) **********" << endl;
+   sd.print();
+   cout << "********* real array **********" << endl;
+   sd.real_print();
+   
+    
+   return 0;
+}
+
+
